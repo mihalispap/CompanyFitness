4811484b4b54eea01b0636f61dad1b67dd5c0416
MihalisP <mihalispapak@gmail.com>
2022-06-20 19:37:35 +0300

    add fitness upload url

--- CompanyFitness/urls.py
+++ CompanyFitness/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app import forms
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('fitness-upload/', forms.FitnessWizard.as_view([forms.FitnessWizardUploadImage, forms.FitnessStatForm])),
 ]
