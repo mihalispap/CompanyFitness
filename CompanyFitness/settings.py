4dac6266aa2be883d3662971e5ad015d8247cb7d
MihalisP <mihalispapak@gmail.com>
2022-06-22 17:26:36 +0300

    fix media upload var

--- CompanyFitness/settings.py
+++ CompanyFitness/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from os import getenv
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'app',
 ]
 
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = getenv('MEDIA_ROOT', 'media/')
+MEDIA_URL = getenv('MEDIA_URL', 'screenshots/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
