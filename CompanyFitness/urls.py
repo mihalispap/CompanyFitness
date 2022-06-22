a73ac5cd47490dfc459e16dbae0219affcfc890c
MihalisP <mihalispapak@gmail.com>
2022-06-22 17:27:22 +0300

    add identify endpoint to urls

--- CompanyFitness/urls.py
+++ CompanyFitness/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from CompanyFitness import settings
 from app import forms
+from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('fitness-upload/', forms.FitnessWizard.as_view([forms.FitnessWizardUploadImage, forms.FitnessStatForm])),
-]
+                  path('admin/', admin.site.urls),
+                  path('identify', views.identify_fitness_metric),
+                  path('fitness-upload/',
+                       forms.FitnessWizard.as_view([forms.FitnessWizardUploadImage, forms.FitnessStatForm])),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
